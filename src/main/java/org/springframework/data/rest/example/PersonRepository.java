61f71b68162f49bf8e33372f7eaee69e2e95a4c0
Jon Brisbin <jon@jbrisbin.com>
2012-05-03 10:05:24 -0500

    Updated things to SD commons 1.3.0.RC1 and SD JPA 1.1.0.BUILD-SNAPSHOT

--- src/main/java/org/springframework/data/rest/example/PersonRepository.java
+++ src/main/java/org/springframework/data/rest/example/PersonRepository.java
@@ -1,9 +1,11 @@
 package org.springframework.data.rest.example;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.repository.annotation.RestResource;
 
 /**
  * @author Jon Brisbin <jon@jbrisbin.com>
  */
+@RestResource(path = "people")
 public interface PersonRepository extends CrudRepository<Person, Long> {
 }
